84804f60ad0ed9ef03a53e018d381ff23f93393c
unknown <rankjay0101@gmail.com>
2023-04-26 15:08:35 +0800

    err code 500 response

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -4,7 +4,7 @@ import { Request, Response } from "express";
 export default async function handler(req: Request, res: Response) {
   const { prompt, clerk_id } = req.body;
 
-  axios.post(
+  return axios.post(
     "https://silicai-server-0sdj.zeet-silicai.zeet.app/api/user/generate",
     {
       clerk_id,
@@ -13,6 +13,9 @@ export default async function handler(req: Request, res: Response) {
   ).then((resp)=> {
     return res.status(200).json({ image: resp.data.image.image });
   }).catch((err) => {
+    if(err.response.status === 500) {
+      return res.status(400).json({ error: "Unfortunately, we are at the system capacity. We are working on scaling our systems. Try again or please check back soon!" });
+    }
     console.log(err.response.data.message);
     return res.status(400).json({ error: err.response.data.message });
   });
