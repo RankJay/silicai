eca0a0fb923782e020084a020a02175d5a1a5671
unknown <rankjay0101@gmail.com>
2023-05-04 12:14:25 +0530

    new price

--- src/pages/components/promptMenu/buy.button.tsx
+++ src/pages/components/promptMenu/buy.button.tsx
@@ -54,7 +54,7 @@ const BuyButton = () => {
         image: snap.checkoutURL,
         imageId: "TBA",
         quantity: 1,
-        price: 1,
+        price: 35,
         metadata: {
           size: event1.value,
           style: event2.value,
