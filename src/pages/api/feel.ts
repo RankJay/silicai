bb6de5b7f124b721e542adb9752db5e115df300a
unknown <rankjay0101@gmail.com>
2023-05-06 11:06:42 +0530

    fixing the API bug

--- src/pages/api/feel.ts
+++ src/pages/api/feel.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { Request, Response } from "express";
 
 export default async function handler(req: Request, res: Response) {
-  const { image_id, clerk_id, isLike } = req.body;
+  const { image_id, clerk_id, isLike } = JSON.parse(req.body);
 
   axios.post(
     "https://silicai-server-0sdj.zeet-silicai.zeet.app/api/user/feel",
