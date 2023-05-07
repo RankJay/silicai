d09bc3764587b5c6a2df145eb9206e09ddca851d
unknown <rankjay0101@gmail.com>
2023-05-07 22:41:26 +0530

    SEO and price update

--- src/pages/closet/[id].tsx
+++ src/pages/closet/[id].tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { useUser } from "@clerk/nextjs";
+import Head from "next/head";
 
 interface UserObjects {
   user_id: string;
@@ -93,6 +94,77 @@ export default function Generated({ user }: { user: UserObjects }) {
 
   return (
     <>
+    <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="keywords"
+          content="AI fashion, fashion trends, fashion technology, AI algorithms, innovative designs"
+        />
+
+        <title>Silic AI : Closet</title>
+        <meta
+          name="description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta name="author" content="Jay Rank" />
+
+        {/* Google / Search Engine Tags */}
+        <meta
+          itemProp="name"
+          content="Silic.AI: Generative Fashion For Real Clothing : Closet"
+        />
+        <meta
+          itemProp="description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta
+          itemProp="image"
+          content="https://user-images.githubusercontent.com/53647573/235449155-661c2a93-9486-46ce-ad27-e2498aae68a0.png"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta
+          property="og:title"
+          content="Silic.AI: Generative Fashion For Real Clothing : Closet"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://silic.ai/" />
+        <meta
+          property="og:description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/53647573/235449155-661c2a93-9486-46ce-ad27-e2498aae68a0.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="" />
+        <meta
+          name="twitter:title"
+          content="Silic.AI: Generative Fashion For Real Clothing : Closet"
+        />
+        <meta
+          name="twitter:description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta
+          name="twitter:image"
+          content="https://user-images.githubusercontent.com/53647573/235449155-661c2a93-9486-46ce-ad27-e2498aae68a0.png"
+        />
+        <meta name="twitter:creator" content="@RankJay1" />
+
+        <meta name="robots" content="index,follow" />
+
+        <link rel="shortcut icon" href="../f2.ico" />
+        {/* <link rel="icon" href="" type="image/svg+xml" /> */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="manifest" href="" />
+        <link rel="apple-touch-icon" href="" />
+      </Head>
       {!isLoading && (
         <div className={styles.closetPageLandingSection}>
           <div className={styles.closetPageCard}>
--- src/pages/design/[id].tsx
+++ src/pages/design/[id].tsx
@@ -8,6 +8,7 @@ import store from "@/store";
 import BuyButton from "../components/promptMenu/buy.button";
 import LikeButton from "../components/promptMenu/like.button";
 import ShareButton from "../components/promptMenu/share.button";
+import Head from "next/head";
 
 interface InventoryObjects {
   image_id: string;
@@ -72,6 +73,77 @@ export default function Generated({ image_data }: { image_data: InventoryObjects
 
   return (
     <>
+    <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="keywords"
+          content="AI fashion, fashion trends, fashion technology, AI algorithms, innovative designs"
+        />
+
+        <title>Silic AI : Design</title>
+        <meta
+          name="description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta name="author" content="Jay Rank" />
+
+        {/* Google / Search Engine Tags */}
+        <meta
+          itemProp="name"
+          content="Silic.AI: Generative Fashion For Real Clothing : Design"
+        />
+        <meta
+          itemProp="description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta
+          itemProp="image"
+          content="https://user-images.githubusercontent.com/53647573/235449155-661c2a93-9486-46ce-ad27-e2498aae68a0.png"
+        />
+
+        {/* Facebook Meta Tags */}
+        <meta
+          property="og:title"
+          content="Silic.AI: Generative Fashion For Real Clothing : Design"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://silic.ai/" />
+        <meta
+          property="og:description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta
+          property="og:image"
+          content="https://user-images.githubusercontent.com/53647573/235449155-661c2a93-9486-46ce-ad27-e2498aae68a0.png"
+        />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:url" content="" />
+        <meta
+          name="twitter:title"
+          content="Silic.AI: Generative Fashion For Real Clothing : Design"
+        />
+        <meta
+          name="twitter:description"
+          content="Revolutionize Your Fabric Designs with Generative AI: Unlock Infinite Creativity and Unique Patterns for Stunning Prints"
+        />
+        <meta
+          name="twitter:image"
+          content="https://user-images.githubusercontent.com/53647573/235449155-661c2a93-9486-46ce-ad27-e2498aae68a0.png"
+        />
+        <meta name="twitter:creator" content="@RankJay1" />
+
+        <meta name="robots" content="index,follow" />
+
+        <link rel="shortcut icon" href="../f2.ico" />
+        {/* <link rel="icon" href="" type="image/svg+xml" /> */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <link rel="manifest" href="" />
+        <link rel="apple-touch-icon" href="" />
+      </Head>
       {needData && (
         <div className={styles.designPageLandingSection}>
           <div className={styles.notFoundBanner}>Loading...</div>
