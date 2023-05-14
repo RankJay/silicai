c84ea0dddb84e622ebf1fc5bcc32ec35796ae32a
unknown <rankjay0101@gmail.com>
2023-05-14 16:53:59 +0530

    adding wombo api

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -9,6 +9,7 @@ export default async function handler(req: Request, res: Response) {
     {
       clerk_id,
       prompt,
+      wombo: true,
     }
   ).then((resp)=> {
     return res.status(200).json({ image: resp.data.image.image });
